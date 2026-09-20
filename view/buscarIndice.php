185046df625a9a29549245b3d4b16e35db0fd101
Edgar Apaza Choque <edgarapazac@gmail.com>
2026-09-20 10:04:47 -0500

    Modificaciones en la organizacion y actualizacion de ventanas
    Se ha cambiado los estilos CSS especialmente de la ventana del
    buscador, asi como del buscadir de otorgantes y los favorecidos
    
    Queda pendiente: modificar la conexion y archivos en PHP8.4
    porque esta esta en la version 5.6.  Se han modificado cosas
    sencillas y sigue funcionando todo, sin embargo, esta trabajando
    con comandos PHP viejos.

--- view/buscarIndice.php
+++ view/buscarIndice.php
@@ -4,108 +4,73 @@ require_once "header.php";
 ?>
 
 <div class="container">
-	<div class="row">
-		<h3 class="alert alert-success">Buscador de Índices</h3>
-	</div>
-	<div class="row">
+  <div class="row">
+    <h3 class="alert alert-success">Buscador de Índices - HUANCANE</h3>
+  </div>
 
-		<div class="col">
+  <div class="row">
+    <div class="col">
+      <form class="form-inline" action='buscarTodo.php' method='post' name='fregistro' id='fregistro' role='form'>
+        <div class="form-group">
+          <button type="submit" name="todo" value="todo" href="#listaIndice.php"  class="btn btn-primary" > <span class="glyphicon glyphicon-search"></span> Buscar Todo</button>
+        </div>
+      </form>
 
-			<?php
-            if (isset($_SESSION['mensaje']) and $_SESSION['mensaje'] != '') {
-                ?>
-				<div class="alert alert-info">
-					<?php echo $_SESSION['mensaje'];
-                unset($_SESSION['mensaje']);?>
-				</div>
-				<?php
-            }
-?>
+      <table>
+        <thead>
+          <tr>
+            <th>Descripcion</th>
+            <th>form</th>
+            <th>Boton</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <form class="form-inline" action='buscarOtorgante.php' method='post' name='fregistro' id='fregistro' role='form'>
+              <td><label class="formGroupExampleInput" for="txtOtorgante">Buscar por Otorgante :</label></td>
+              <td><input type="search" class="form-control" id="txtOtorgante" name="txtOtorgante" placeholder=""></td>
+              <td><button type="submit" name="otorgante" value="otorgante" class="btn btn-primary" > <span class="glyphicon glyphicon-search"></span> Buscar Por Otorgante</button>
+</td>
+            </form>
+          </tr>
+          <tr>
+            <form class="form-inline" action='buscarOtorgante.php' method='post' name='fregistro' id='fregistro' role='form'>
+              <td>Buscar por Otorgante:</td>
+              <td><input type="search" class="form-control" id="txtOtorgante" name="txtOtorgante" placeholder="">
+</td>
+              <td><button type="submit" name="otorgante" value="otorgante" class="btn btn-primary" > <span class="glyphicon glyphicon-search"></span> Buscar Por Otorgante</button>
+</td>
+            </form>
+          </tr>
+          <tr>
+            <form class="form-inline" action='buscarFavorecido.php' method='post' name='fregistro' id='fregistro' role='form'>
+              <td>Buscar por Favorecido</td>
+              <td><input type="text" class="form-control" id="txtFavorecido" name="txtFavorecido" placeholder="">
+</td>
+              <td><button type="submit" name="favorecido" value="favorecido" class="btn btn-primary" > <span class="glyphicon glyphicon-search"></span> Buscar Por Favorecido</button>
+</td>
+            </form>
+          </tr>
+          <tr>
+            <form class="form-inline" action='buscarFecha.php' method='post' name='fregistro' id='fregistro' role='form'>
+              <td>Buscar por Fecha:</td>
+              <td><input type="date" class="form-control" id="txtFecha" name="txtFecha" >
+</td>
+              <td><button type="submit" name="fecha" value="fecha" class="btn btn-primary" > <span class="glyphicon glyphicon-search"></span> Buscar Por Fecha</button>
+</td>
+            </form>
+          </tr>
+          <tr>
+            <form class="form-inline" action='buscarBien.php' method='post' name='fregistro' id='fregistro' role='form'>
+              <td>Buscar por Nombre del Bien:</td>
+              <td><input type="text" class="form-control" id="txtBien" name="txtBien" placeholder="">
+</td>
+              <td><button type="submit" name="bien" value="bien" class="btn btn-primary" > <span class="glyphicon glyphicon-search"></span> Buscar Por Nombre del Bien</button>
+</td>
+            </form>
+          </tr>
 
-			<form class="form-inline" action='buscarTodo.php' method='post' name='fregistro' id='fregistro' role='form'>
-				<div class="form-group">
-					<div class="">
-						<button type="submit" name="todo" value="todo" href="#listaIndice.php"  class="btn btn-primary" > <span class="glyphicon glyphicon-search"></span> Buscar Todo</button>
-					</div>
-				</div>
-			</form>
-
-			<form class="form-inline" action='buscarNotario.php' method='post' name='fregistro' id='fregistro' role='form'>
-				<div class="form-group">
-					<label class="formGroupExampleInput" for="txtNotario">Buscar por Notario :</label>
-
-						<select class="form-control" id="txtNotario" name="txtNotario" >
-							<option value="0">SELECCIONE NOTARIO</option>
-							<option value="55">Luis Jimenez Vargas (Huancane)</option>
-							<option value="21">Miguel Pino Chavez (Puno)</option>
-							<option value="100">Hector Garnica Rosado (Puno)</option>
-						</select>
-
-
-						<button type="submit" name="notario" value="notario" class="btn btn-primary" > <span class="glyphicon glyphicon-search"></span> Buscar Por Notario</button>
-
-
-				</div>
-			</form>
-
-			<form class="form-inline" action='buscarOtorgante.php' method='post' name='fregistro' id='fregistro' role='form'>
-				<div class="form-group">
-					<label class="formGroupExampleInput" for="txtOtorgante">Buscar por Otorgante :</label>
-
-						<input type="search" class="form-control" id="txtOtorgante" name="txtOtorgante" placeholder="Ingrese el Otorgante">
-
-					<div class="form-group">
-						<button type="submit" name="otorgante" value="otorgante" class="btn btn-primary" > <span class="glyphicon glyphicon-search"></span> Buscar Por Otorgante</button>
-					</div>
-				</div>
-			</form>
-
-			<form class="form-inline" action='buscarFavorecido.php' method='post' name='fregistro' id='fregistro' role='form'>
-				<div class="form-group">
-					<label class="formGroupExampleInput" for="txtFavorecido">Buscar por Favorecido :</label>
-
-						<input type="text" class="form-control" id="txtFavorecido" name="txtFavorecido" placeholder="Ingrese el Favorecido">
-
-						<button type="submit" name="favorecido" value="favorecido" class="btn btn-primary" > <span class="glyphicon glyphicon-search"></span> Buscar Por Favorecido</button>
-
-				</div>
-			</form>
-
-			<form class="form-inline" action='buscarFecha.php' method='post' name='fregistro' id='fregistro' role='form'>
-				<div class="form-group">
-					<label class="formGroupExampleInput" for="txtFecha">Buscar por Fecha :</label>
-
-						<input type="date" class="form-control" id="txtFecha" name="txtFecha" placeholder="<?php echo $fecha = date('Y-m-d');?>" maxlength="10">Formato: Año-Mes-Día
-
-						<button type="submit" name="fecha" value="fecha" class="btn btn-primary" > <span class="glyphicon glyphicon-search"></span> Buscar Por Fecha</button>
-
-				</div>
-			</form>
-
-			<form class="form-inline" action='buscarSerie.php' method='post' name='fregistro' id='fregistro' role='form'>
-				<div class="form-group">
-					<label class="formGroupExampleInput" for="txtSerie">Buscar por Serie :</label>
-
-						<input type="text" class="form-control" id="txtSerie" name="txtSerie" placeholder="Ingrese la Serie">
-
-						<button type="submit" name="serie" value="serie" class="btn btn-primary" > <span class="glyphicon glyphicon-search"></span> Buscar Por Serie</button>
-
-				</div>
-			</form>
-
-			<form class="form-inline" action='buscarBien.php' method='post' name='fregistro' id='fregistro' role='form'>
-				<div class="form-group">
-					<label class="formGroupExampleInput" for="txtBien">Buscar por Nombre del Bien :</label>
-
-						<input type="text" class="form-control" id="txtBien" name="txtBien" placeholder="Ingrese el Nombre del Bien">
-
-						<button type="submit" name="bien" value="bien" class="btn btn-primary" > <span class="glyphicon glyphicon-search"></span> Buscar Por Nombre del Bien</button>
-
-				</div>
-			</form>
-
-		</div>
-
-	</div>
-</div>
-
+        </tbody>
+      </table>
+    </div>
+  </div>
--- view/buscarOtorgante.php
+++ view/buscarOtorgante.php
@@ -22,63 +22,51 @@ $total  = $result->num_rows;
 
 <div class="container-fluid">
 
-	<div class="row">
-		<div class="col-md-12">
+  <div class="row">
+    <div class="col-md-12">
+      <center><h3>Lista de Índices por Otorgantes</h3></center>
+      <?php echo "<h4>Número de Índices Encontrados: $total datos encontrados.</h4> " ?>
 
-			<center><h3>Lista de Índices por Otorgantes</h3></center>
-			<?php echo "<h4>Número de Índices Encontrados: $total Índices</h4> " ?>
-
-			<table class="table table-striped table-bordered table-responsive" >
-				<thead>
-					<tr>
-						<th>Número</th>
-						<th>Nombre Notario</th>
-						<th>Otorgante</th>
-						<th>Favorecido</th>
-						<th>Fecha</th>
-						<th>Serie</th>
-						<th>Folio</th>
-						<th>Escritura</th>
-						<th>Nombre del Bien</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php
-                    $_SESSION['oPDF'] = [];
+      <table class="table table-striped table-bordered table-responsive" >
+        <thead>
+          <tr>
+            <th>Núm</th>
+            <th>Nombre Notario</th>
+            <th>Otorgante</th>
+            <th>Favorecido</th>
+            <th>Fecha</th>
+            <th>Serie</th>
+            <th>Folio</th>
+            <th>Escritura</th>
+            <th>Nombre del Bien</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+            $i = 1;
+$_SESSION['oPDF'] = [];
 while ($lista1 = $result->fetch_assoc()) {
     $_SESSION['oPDF'][] = $lista1;
     ?>
-						<tr>
-							<td><?php echo $lista1['codIndice'];?></td>
-							<td><?php echo $lista1['notario'];?></td>
-							<td><?php echo $lista1['otorgante'];?></td>
-							<td><?php echo $lista1['favorecido'];?></td>
-							<td><?php echo $lista1['fecha'];?></td>
-							<td><?php echo $lista1['subserie'];?></td>
-							<td><?php echo $lista1['folio'];?></td>
-							<td><?php echo $lista1['escritura'];?></td>
-							<td><?php echo $lista1['bien'];?></td>
 
-							<td>
-								<a href="#"><span class="glyphicon glyphicon-edit "></span>Editar</a>
+          <tr>
+            <td><?php echo $i;?></td>
+            <td><?php echo $lista1['notario'];?></td>
+            <td><?php echo $lista1['otorgante'];?></td>
+            <td><?php echo $lista1['favorecido'];?></td>
+            <td><?php echo $lista1['fecha'];?></td>
+            <td><?php echo $lista1['subserie'];?></td>
+            <td><?php echo $lista1['folio'];?></td>
+            <td><?php echo $lista1['escritura'];?></td>
+            <td><?php echo $lista1['bien'];?></td>
+          </tr>
+          <?php
+            $i++;
+}    ?>
+        </tbody>
+      </table>
 
-							</td>
-						</tr>
-						<?php
-}
-?>
-				</tbody>
-				<tfoot>
-
-				</tfoot>
-			</table>
-
-			<div class="btn-group" role="group" aria-label="...">
-				<a href="listado.imprimir.php" class="btn btn-default" target="_blank"><span class="glyphicon glyphicon-print" aria-hidden="true"></span> Imprimir Lista</a>
-
-			</div>
-
-		</div>
-	</div>
+    </div>
+  </div>
 
 </div>
